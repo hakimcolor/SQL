4152e2d3f739724771349838584d317b07443231
hakimcolor <hakimcolor777@gmail.com>
2026-07-11 21:23:59 +0600

    using alter update name create column .. update column deleate column

--- alter.sql
+++ alter.sql
@@ -1,8 +1,20 @@
 --create table
-create table emplye(
-  id serial, name varchar(52), age int
-)
+CREATE TABLE emplye (id serial, name varchar(52), age int)
 --Renaming table name 
-alter table emplye rename to employee
+ALTER TABLE emplye
+RENAME TO employee
+--add a colum in a table
+ALTER TABLE employee
+ADD COLUMN email varchar(50);
+
+
+--Drop a column;
+--ALTER TABLE employee
+--DROP COLUMN email;
+
+-- reanameing a column 
+alter table employee rename name to user_name;
+
+
 
 
