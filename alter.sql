c983962d5aa76a797d2c6feb767dc35b38ca9c6e
hakimcolor <hakimcolor777@gmail.com>
2026-07-12 22:04:57 +0600

    sql  add constraint value

--- alter.sql
+++ alter.sql
@@ -21,3 +21,36 @@ alter table employee alter column email set not null;
 
 --drop constraint
 alter table employee alter column email drop not null;
+
+--set defult value for a column 
+insert into employee(user_name, age, email) values('hakimali', 22, 'hakimcoloer777@gmail.com');
+alter table employee alter column email set default 'stest@gmail.com'
+
+  --Remove defult value
+  alter table employee alter column email drop default;
+  
+-- add constraint table level constraint
+alter table employee add constraint u_e_email unique(email);
+
+
+-- drop constraint table leverl constraint 
+alter table employee drop constraint u_e_email ;
+
+-- add prymary key ..
+alter table employee add constraint pk_emp_id primary key(id);
+
+-- drop prymary key 
+alter table employee drop constraint pk_emp_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
