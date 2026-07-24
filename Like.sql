06829e67d4753cfac718d92c89238750fa3d5c96
hakimcolor <hakimcolor777@gmail.com>
2026-07-24 11:02:39 +0600

    not ope

--- Like.sql
+++ Like.sql
@@ -1,7 +1,7 @@
 --select student whose first name starts with 'A'
  select * from students where first_name like 'A%'
  select * from students where first_name like '__a%'
-select * from students where first_name like '%a'
+select * from students where first_name like '%a'-- end a 
  select * from students where first_name like '%a_'
  select * from students where first_name like '%a___'
 -- all small or upper.. any thing 
---
+++ NOt_ope.sql
@@ -0,0 +1,4 @@
+--NOT 
+--Select students who are not from bd ,
+ select * from students where country !='Bangladesh'
+select * from students where not country !='Bangladesh'
