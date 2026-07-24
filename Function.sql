31554d28660c05d491c2c8b14f4cf4ee53e2b031
hakimcolor <hakimcolor777@gmail.com>
2026-07-24 23:27:06 +0600

    coalesce

--- Function.sql
+++ Function.sql
@@ -6,3 +6,11 @@ select lower(first_name) as "upFNM" ,first_name, age from students
 --concat or add two or more 
 select concat(first_name, last_name) as "Full Nm" ,first_name, age from students
 
+
+
+-- select null 
+select * from students where email is not null
+
+-- coalesce ..it's like placeholder
+select first_name, age, coalesce(email, 'provite your email ') as email from students
+
